a2e689c93eaaacdf32443fac2df42b0b36c93707
Istvan Deak <dexterslabor@gmail.com>
2020-10-15 16:29:49 +0200

    fix(tzip7 migration): update tzip7 migration file

--- migrations/2_deploy_tzip7.js
+++ migrations/2_deploy_tzip7.js
@@ -3,30 +3,24 @@ const { alice } = require('./../scripts/sandbox/accounts');
 const { MichelsonMap } = require('@taquito/taquito');
 const saveContractAddress = require('./../helpers/saveContractAddress');
 
-
-const initial_storage = {
-    token : {
-        ledger:new MichelsonMap,
+const initialStorage = {
+    token: {
+        ledger: MichelsonMap.fromLiteral({
+            [alice.pkh]: 10, 
+        }),
         approvals: new MichelsonMap,
         admin: alice.pkh,
         paused: false,
         totalSupply: 100,
     },
-    swaps 	     : new MichelsonMap,
-    hashlock     : new MichelsonMap,
-    status	     : new MichelsonMap,
-    secrets      : new MichelsonMap,
-    bridge       : {
-        swaps       : new MichelsonMap,
-        outcomes    : new MichelsonMap
-    }
-}
+    bridge: {
+        swaps: new MichelsonMap,
+        outcomes: new MichelsonMap
+    },
+};
 
 module.exports = async (deployer, network, accounts) => {
-
-    // TODO format to await instead of .then
-    deployer.deploy(tzip7, initial_storage)
+    deployer.deploy(tzip7, initialStorage)
         .then(contract => saveContractAddress('tzip7', contract.address));
-
 };
-module.exports.initial_storage = initial_storage;
+module.exports.initialStorage = initialStorage;
