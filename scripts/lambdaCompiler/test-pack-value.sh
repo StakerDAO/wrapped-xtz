37466adf9f79367c36591f1b30cd92af2e4f3770
István Deák <dexterslabor@gmail.com>
2020-11-06 18:48:54 +0100

    ci(lambda script): remove global ligo dependency and add JQ install to CI job

--- scripts/lambdaCompiler/test-pack-value.sh
+++ scripts/lambdaCompiler/test-pack-value.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 EXPRESSION="$1"
-BYTES=`ligo interpret --syntax=reasonligo "Bytes.pack($EXPRESSION)"`
+BYTES=`docker run -v $PWD:/usr/project --rm -i ligolang/ligo:next interpret --syntax=reasonligo "Bytes.pack($EXPRESSION)"`
 
 OUTPUT=$(cat <<-END
 {
