0484a279d95b72c7256c2d710237e6bf38d7e4ab
Matej Sima <matej.sima@gmail.com>
2020-11-12 17:51:55 +0100

    fix(migrations): updated core base migration

--- migrations/initialStorage/core.js
+++ migrations/initialStorage/core.js
@@ -5,37 +5,37 @@ const { alice } = require('./../../scripts/sandbox/accounts');
 const lambdasList = require('./../../lambdas');
 const loadLambdaArtifact = require('./../../scripts/lambdaCompiler/loadLambdaArtifact');
 
-let lambdas = new MichelsonMap;
-let ovens = new MichelsonMap;
-let arbitraryValues = new MichelsonMap;
+const initialStorage = {};
 
-arbitraryValues.set('wXTZTokenContractAddress', 
-    testPackValue(
-        `"${require('../../deployments/tzip-7')}": address`
-    )
-);
+initialStorage.base = (() => {
+    let lambdas = new MichelsonMap;
+    let ovens = new MichelsonMap;
+    let arbitraryValues = new MichelsonMap;
 
-arbitraryValues.set('admin', 
-    testPackValue(
-        `"${alice.pkh}": address`
-    )
-);
+    arbitraryValues.set('wXTZTokenContractAddress', 
+        testPackValue(
+            `"${require('../../deployments/tzip-7')}": address`
+        )
+    );
 
-lambdasList.forEach(lambda => {
-    if (lambda.migratable === false) return;
-    // TODO: instead of `lambda.recompile` hash the lambda and recompile incrementally by itself
-    let lambdaArtifact = loadLambdaArtifact(lambda.lambdaName, lambda.recompile);
-    lambdas.set(lambda.lambdaAlias, lambdaArtifact.bytes);
-});
+    arbitraryValues.set('admin', 
+        testPackValue(
+            `"${alice.pkh}": address`
+        )
+    );
 
+    lambdasList.forEach(lambda => {
+        if (lambda.migratable === false) return;
+        // TODO: instead of `lambda.recompile` hash the lambda and recompile incrementally by itself
+        let lambdaArtifact = loadLambdaArtifact(lambda.lambdaName, lambda.recompile);
+        lambdas.set(lambda.lambdaAlias, lambdaArtifact.bytes);
+    });
 
-const initalStorage = {};
+    return {
+        lambdas,
+        ovens,
+        arbitraryValues,
+    }
+})();
 
-initalStorage.base = {
-    u: UnitValue,
-    lambdas,
-    ovens,
-    arbitraryValues
-};
-
-module.exports = initalStorage;
+module.exports = initialStorage;
