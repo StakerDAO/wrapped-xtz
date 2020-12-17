c924e688025240b1fa6cc1b7c2bdce42daeab388
dexterslabor <dexterslabor@gmail.com>
2020-12-17 14:05:42 +0000

    refactor(helper): update helper function to calculate delayed timestamps

--- helpers/getDelayedISOTime.js
+++ helpers/getDelayedISOTime.js
@@ -1,22 +1,9 @@
-function convertMinutes(totalMinutes) {
-    const hoursWithRemainder = (totalMinutes / 60);
-    // remove hours remainder by rounding down
-    const hours = Math.floor(hoursWithRemainder);
-    const minutesWithRemainder = (hoursWithRemainder - hours) * 60;
-    // remove minutes remainder by rounding up
-    const minutes = Math.round(minutesWithRemainder);
-    return { hours, minutes }
-};
-
-module.exports = (totalMinutes) => {
-    const { hours, minutes } = convertMinutes(totalMinutes);
-    const timeNow = new Date();
-    // set new time
-    const delayedHour = timeNow.getHours() + hours;
-    const delayedMinutes = timeNow.getMinutes() + minutes;
-    timeNow.setHours(delayedHour, delayedMinutes);
+module.exports = (minutes) => {
+    let unixEpoch = Date.now();
+    const delayInMilliseconds = minutes * 60 * 1000;
+    unixEpoch += delayInMilliseconds;
+    const timeWithDelay = new Date(unixEpoch);
     // Remove milliseconds for Tezos protocol
-    timeNow.setMilliseconds(000);
-    const timeWithDelay = timeNow.toISOString();
-    return timeWithDelay
+    timeWithDelay.setMilliseconds(000);
+    return timeWithDelay.toISOString()
 };
